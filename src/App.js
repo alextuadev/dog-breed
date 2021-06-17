40d661a98cdc6d2b5832312cb545ef492678266b
Alexis Borges <tuaalexis@gmail.com>
2021-06-16 21:01:22 -0600

    Inicializacion y puesta en marcha de las primeras pantallas e integracion con Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Breeds from './containers/Breeds';
+import Home from './containers/Home';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <ul className="nav">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/breeds">Breeds</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/breeds">
+              <Breeds />
+            </Route>
+          </Switch>
+        </div>
+
+      </Router>
+    </>
   );
 }
+
 
 export default App;
